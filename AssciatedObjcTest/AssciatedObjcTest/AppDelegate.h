3277603892ecffe74e3ab5d8bfeab9dc4560197e
吴涛昌 <wtc@ekuaibao.com>
2016-11-28 10:08:44 +0800

    试试

---
+++ AssciatedObjcTest/AssciatedObjcTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssciatedObjcTest
+//
+//  Created by Toge Wu on 11/25/16.
+//  Copyright © 2016 Toge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
